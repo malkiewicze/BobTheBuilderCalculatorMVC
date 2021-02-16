6f7e307d5505042b8f4e4a46846bfbdb38df263b
malkiewicze <malkiewicze@gmail.com>
2021-02-16 20:29:04 +0100

    Add new files

---
+++ BobTheBuilderCalculatorMVC.Web/Models/RoomModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BobTheBuilderCalculatorMVC.Web.Models
+{
+    public class RoomModel
+    {
+        public string RoomName { get; set; }
+        public double Length { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public int ID { get; set; }
+    }
+}
